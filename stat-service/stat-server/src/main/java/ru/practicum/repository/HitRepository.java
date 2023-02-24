1a2885a3c9f644676b44dfe495d6538f4fac19a8
=AlexSharygin <AlexSharygin1989@yandex.ru>
2023-02-24 20:33:09 +0000

    fixes

--- stat-service/stat-server/src/main/java/ru/practicum/repository/HitRepository.java
+++ stat-service/stat-server/src/main/java/ru/practicum/repository/HitRepository.java
@@ -14,28 +14,28 @@ public interface HitRepository extends JpaRepository<Hit, Long> {
 
     @Query("SELECT new ru.practicum.stats.Stats(a.name, h.uri, COUNT (h.id)) " +
             "FROM Hit h JOIN App a ON a.id=h.app.id " +
-            "WHERE h.date_time BETWEEN :startDateTime AND :endDateTime " +
+            "WHERE h.dateTime BETWEEN :startDateTime AND :endDateTime " +
             "GROUP BY  h.uri, a.name " +
             "ORDER BY COUNT(h.id) DESC")
     List<Stats> findAllHits(LocalDateTime startDateTime, LocalDateTime endDateTime);
 
     @Query("SELECT new ru.practicum.stats.Stats(a.name, h.uri, count (h.id))" +
             "FROM Hit h JOIN App a ON a.id=h.app.id " +
-            "WHERE h.date_time BETWEEN :startDateTime AND :endDateTime " +
+            "WHERE h.dateTime BETWEEN :startDateTime AND :endDateTime " +
             "GROUP BY h.uri, h.ip " +
             "ORDER BY COUNT(h.id) DESC")
     List<Stats> findAllUniqueHits(LocalDateTime startDateTime, LocalDateTime endDateTime);
 
     @Query("SELECT new ru.practicum.stats.Stats(a.name, h.uri, count (h.id))" +
             "FROM Hit h JOIN App a ON a.id=h.app.id " +
-            "WHERE (h.date_time BETWEEN :startDateTime AND :endDateTime) AND (h.uri IN (:uris))" +
+            "WHERE (h.dateTime BETWEEN :startDateTime AND :endDateTime) AND (h.uri IN (:uris))" +
             "GROUP BY h.uri,a.name " +
             "ORDER BY COUNT(h.id) DESC")
     List<Stats> findHitsByUris(LocalDateTime startDateTime, LocalDateTime endDateTime, List<String> uris);
 
     @Query("SELECT new ru.practicum.stats.Stats(a.name, h.uri, count (h.id))" +
             "FROM Hit h JOIN App a ON a.id=h.app.id " +
-            "WHERE h.date_time BETWEEN :startDateTime AND :endDateTime AND (h.uri IN (:uris))" +
+            "WHERE h.dateTime BETWEEN :startDateTime AND :endDateTime AND (h.uri IN (:uris))" +
             "GROUP BY h.uri, h.ip " +
             "ORDER BY COUNT (h.id) desc")
     List<Stats> findUniqueHitsByUris(LocalDateTime startDateTime, LocalDateTime endDateTime, List<String> uris);
