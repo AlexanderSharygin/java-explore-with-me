58ba44f07acce44ba271c58a197e9a20d63fc44c
=AlexSharygin <AlexSharygin1989@yandex.ru>
2023-02-24 22:04:03 +0000

    fix

--- stats-service/stats-server/src/main/java/ru/practicum/service/StatService.java
+++ stats-service/stats-server/src/main/java/ru/practicum/service/StatService.java
@@ -3,6 +3,7 @@ package ru.practicum.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import ru.practicum.hit.HitDto;
 import ru.practicum.mapper.HitMapper;
 import ru.practicum.model.App;
